29967e3c28e683ec0978ad51188b64f771082f03
mizon <mizon9@gmail.com>
2013-10-14 22:28:25 +0900

    Stop using defn inside other defn

--- src/wikirick2/repository.clj
+++ src/wikirick2/repository.clj
@@ -5,12 +5,12 @@
             [clojure.string :as string])
   (:import java.util.concurrent.locks.ReentrantReadWriteLock))
 
-(defn- select-article- [repo title co-args]
-  (defn parse-revision [text]
-    (let [[_ message] (string/split-lines text)
-          [_ rev] (re-find #"^revision \d+\.(\d+)$" message)]
-      (Integer. rev)))
+(defn- parse-revision [text]
+  (let [[_ message] (string/split-lines text)
+        [_ rev] (re-find #"^revision \d+\.(\d+)$" message)]
+    (Integer. rev)))
 
+(defn- select-article- [repo title co-args]
   (let [result (apply shell/sh `("co" ~@co-args :dir ~(.base-dir repo)))]
     (if (= (:exit result) 0)
       (let [rev (parse-revision (:err result))
@@ -45,25 +45,23 @@
         (select-article- self title [(format "-r1.%s" rev) "-p" title]))))
 
   (select-all-article-titles [self]
-    (defn ls-rcs-dir []
-      (:out (shell/sh "ls" "-t" (format "%s/RCS" base-dir))))
-
-    (with-read-lock self
-      (fn []
-        (for [rcs-file (string/split-lines (ls-rcs-dir)) :when (not (empty? rcs-file))]
-          (let [[_ article-name] (re-find #"(.+),v" rcs-file)]
-            article-name)))))
+    (letfn [(ls-rcs-dir []
+              (:out (shell/sh "ls" "-t" (format "%s/RCS" base-dir))))]
+      (with-read-lock self
+        (fn []
+          (for [rcs-file (string/split-lines (ls-rcs-dir)) :when (not (empty? rcs-file))]
+            (let [[_ article-name] (re-find #"(.+),v" rcs-file)]
+              article-name))))))
 
   (post-article [self article]
-    (defn check-out-rcs-file []
-      (shell/sh "co" "-l" (.title article) :dir (.base-dir self)))
-
-    (with-write-lock self
-      (fn []
-        (check-out-rcs-file)
-        (let [path (format "%s/%s" base-dir (.title article))]
-          (spit path (.source article)))
-        (shell/sh "ci" (.title article) :in (.edit-comment article) :dir base-dir)))))
+    (letfn [(check-out-rcs-file []
+              (shell/sh "co" "-l" (.title article) :dir (.base-dir self)))]
+      (with-write-lock self
+        (fn []
+          (check-out-rcs-file)
+          (let [path (format "%s/%s" base-dir (.title article))]
+            (spit path (.source article)))
+          (shell/sh "ci" (.title article) :in (.edit-comment article) :dir base-dir))))))
 
 (defn create-repository [base-dir]
   (shell/sh "mkdir" "-p" (format "%s/RCS" base-dir))
