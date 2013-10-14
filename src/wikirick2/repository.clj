abec81fc983aadc3505a59065436039154934f10
mizon <mizon9@gmail.com>
2013-10-14 13:00:43 +0900

    Refactoring: Inline helper functions

--- src/wikirick2/repository.clj
+++ src/wikirick2/repository.clj
@@ -6,21 +6,18 @@
             [clojure.string :as string])
   (:import java.io.File))
 
-(defn- parse-revision [text]
-  (let [[_ message] (string/split-lines text)
-        [_ rev] (re-find #"^revision \d+\.(\d+)$" message)]
-    (Integer. rev)))
-
 (defn- select-article- [repo title co-args]
+  (defn parse-revision [text]
+    (let [[_ message] (string/split-lines text)
+          [_ rev] (re-find #"^revision \d+\.(\d+)$" message)]
+      (Integer. rev)))
+
   (let [result (apply shell/sh `("co" ~@co-args :dir ~(.base-dir repo)))]
     (if (= (:exit result) 0)
       (let [rev (parse-revision (:err result))
             article (make-article title (:out result))]
         (assoc article :revision rev))
       (throw (RuntimeException. (:err result))))))
-
-(defn- check-out-rcs-file [repo article]
-  (shell/sh "co" "-l" (.title article) :dir (.base-dir repo)))
 
 (deftype Repository [base-dir]
   IRepository
@@ -36,7 +33,10 @@
         article-name)))
 
   (post-article [self article]
-    (check-out-rcs-file self article)
+    (defn check-out-rcs-file []
+      (shell/sh "co" "-l" (.title article) :dir (.base-dir self)))
+
+    (check-out-rcs-file)
     (let [path (format "%s/%s" base-dir (.title article))]
       (spit path (.source article)))
     (shell/sh "ci" (.title article) :in (.edit-comment article) :dir base-dir)))
