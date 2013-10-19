8b2f05c01784a70f170fb10fc68a60fad1dbe876
mizon <mizon9@gmail.com>
2013-10-19 18:21:32 +0900

    Add some package dependencies

--- project.clj
+++ project.clj
@@ -3,10 +3,17 @@
   :url "http://mizondev.net/wiki/wikirick2"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]
-                 [parse-ez "0.3.6"]]
+                 [org.clojure/clojurescript "0.0-1934"]
+                 [hiccup "1.0.4"]
+                 [slingshot "0.10.3"]]
   :plugins [[lein-ring "0.8.5"]
-            [speclj "2.5.0"]]
+            [speclj "2.5.0"]
+            [lein-cljsbuild "0.3.4"]]
   :ring {:handler wikirick2.handler/app}
   :profiles {:dev {:dependencies [[ring-mock "0.1.5"]
                                   [speclj "2.5.0"]]}}
-  :test-paths ["spec"])
+  :test-paths ["spec"]
+  :cljsbuild {:builds [{:source-paths ["src-cljs"]
+                        :compiler {:output-to "resources/public/js/index.js"
+                                   :optimizations :whitespace
+                                   :pretty-print true}}]})
