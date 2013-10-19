7155c65c5d3ae955f28f49985bb9dad6cb79fbac
mizon <mizon9@gmail.com>
2013-10-19 18:22:29 +0900

    Add a naive article handler and spec examples of it

--- spec/wikirick2/spec/handler.clj
+++ spec/wikirick2/spec/handler.clj
@@ -1,14 +1,39 @@
 (ns wikirick2.spec.handler
-  (:use speclj.core
-        ring.mock.request
-        wikirick2.handler))
+  (:use ring.mock.request
+        speclj.core
+        wikirick2.handler
+        wikirick2.service
+        wikirick2.types
+        wikirick2.spec.spec-helper)
+  (:require [clojure.java.shell :as shell]
+            [wikirick2.screen :as screen]))
+
+(defn- service [getter]
+  (getter wiki-service))
+
+(defn- should-be-full-rendered [res template]
+  (should= (render-full (service get-screen) template) (res :body)))
 
 (describe "application handler"
+  (after
+    (cleanup-test-repo))
+  (around [example]
+    (binding [wiki-service testing-service]
+      (example)))
+
   (it "handles GET /"
     (let [res (app (request :get "/"))]
-      (should= (:status res) 200)
-      (should= (:body res) "<h1>Hello World</h1>")))
+      (should= (res :status) 200)
+      (should= (res :body) "<h1>Hello World</h1>")))
 
-  (it "handles invalid requests"
-    (let [res (app (request :get "/invalid"))]
-      (should= (:status res) 404))))
+  (context "with two articles"
+    (with foo-page (make-article "FooPage" "some content"))
+    (with bar-page (make-article "FooPage" "some content"))
+    (before
+      (post-article (service get-repository) @foo-page)
+      (post-article (service get-repository) @bar-page))
+
+    (it "handles GET /FooPage"
+      (let [res (app (request :get "/FooPage"))]
+        (should= (res :status) 200)
+        (should-be-full-rendered res (screen/article @foo-page))))))
--- src/wikirick2/handler.clj
+++ src/wikirick2/handler.clj
@@ -1,14 +1,31 @@
 (ns wikirick2.handler
   (:use compojure.core
-        wikirick2.service)
+        wikirick2.service
+        wikirick2.types)
   (:require [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [wikirick2.screen :as screen]))
+
+(def ^:dynamic wiki-service
+  (->WikiService
+   {:repository-dir "repo"
+    :base-path "/"}))
+
+(defn- service [getter]
+  (getter wiki-service))
 
 (defn- handle-route []
   "<h1>Hello World</h1>")
 
+(defn- handle-navigation [])
+
+(defn- handle-article [title]
+  (let [article (select-article (service get-repository) title)]
+    (render-full (service get-screen) (screen/article article))))
+
 (defroutes app-routes
   (GET "/" [] (handle-route))
+  (GET "/:title" [title] (handle-article title))
   (route/resources "/")
   (route/not-found "Not Found"))
 
