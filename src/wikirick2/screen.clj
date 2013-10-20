ddfe19ee9415b7fb2cfbeb81e2e0cf16186495c6
mizon <mizon9@gmail.com>
2013-10-20 20:56:29 +0900

    Design article pages as draft

--- src/wikirick2/screen.clj
+++ src/wikirick2/screen.clj
@@ -4,17 +4,41 @@
   (:require [hiccup.page :as page]))
 
 (defn base [fragment service]
-  (->Template (.title fragment)
-    (let [url-mapper (.url-mapper service)
-          config (.config service)]
-      [:html
-       [:head
-        [:link {:href (css-path url-mapper) :type "text-css" :rel "stylesheet"}]
-        [:title (config :site-title)]]])))
+  (let [urlm (.url-mapper service)
+        config (.config service)]
+    (->Template
+     (.title fragment)
+     [:html
+      [:head
+       [:title (h (config :site-title))]
+       [:link {:rel "stylesheet" :type "text/css" :href (theme-path urlm)}]]
+      [:body
+       [:div#container
+        [:div#wrapper
+         [:div.content
+          [:nav
+           [:p {:class "locator"} "Top > " [:em {:class "selected"} (h (.title fragment))]]
+           [:ul
+            [:li {:class "selected"} "Article"]
+            [:li "Source"]
+            [:li "Edit"]
+            [:li "History"]]]
+          (.body fragment)]
+         [:aside
+          [:header {:id "main-title"} [:h1 (h (config :site-title))]]
+          [:section#search
+           [:form
+            [:input {:class "search-box" :type "text" :placeholder "Search"}]]]
+          [:section
+           [:h2 "Recent Updates"]]]]
+        [:footer "Powered by Clojure Programming Language"]]]])))
 
 (defn article [article-]
-  (->Template (.title article-)
-    [:h3 (.source article-)]))
+  (->Template
+   (.title article-)
+   [:article
+    [:header [:h1 (.title article-)]]
+    [:p (.source article-)]]))
 
 (deftype Screen [service]
   IScreen
