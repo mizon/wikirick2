1d14466c00735419d6ec541d19f214cef104995c
mizon <mizon9@gmail.com>
2013-10-22 00:14:02 +0900

    Refactoring: Move some helper functions to a module

--- src/wikirick2/repository.clj
+++ src/wikirick2/repository.clj
@@ -1,30 +1,10 @@
 (ns wikirick2.repository
-  (:use wikirick2.types
-        slingshot.slingshot)
+  (:use slingshot.slingshot
+        wikirick2.repository.helper
+        wikirick2.types)
   (:require [clojure.java.shell :as shell]
             [clojure.string :as string])
   (:import java.util.concurrent.locks.ReentrantReadWriteLock))
-
-(defn- parse-revision [text]
-  (let [[_ message] (string/split-lines text)
-        [_ rev] (re-find #"^revision \d+\.(\d+)$" message)]
-    (Integer. rev)))
-
-(defn- select-page- [repo title co-args]
-  (let [result (apply shell/sh `("co" ~@co-args :dir ~(.base-dir repo)))]
-    (if (= (:exit result) 0)
-      (let [rev (parse-revision (:err result))
-            page (make-page title (:out result))]
-        (assoc page :revision rev))
-      (throw (RuntimeException. (:err result))))))
-
-(defmacro with-rw-lock [lock-type & forms]
-  `(do
-     (.. ~'rw-lock ~lock-type lock)
-     (try
-       ~@forms
-       (finally
-         (.. ~'rw-lock ~lock-type unlock)))))
 
 (deftype Repository [base-dir rw-lock]
   IRepository
@@ -48,7 +28,7 @@
   (post-page [self page]
     (letfn
       [(check-out-rcs-file []
-         (shell/sh "co" "-l" (.title page) :dir (.base-dir self)))]
+         (shell/sh "co" "-l" (.title page) :dir base-dir))]
       (with-rw-lock writeLock
         (check-out-rcs-file)
         (let [path (format "%s/%s" base-dir (.title page))]
