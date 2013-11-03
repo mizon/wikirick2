fa095214d6167484bb46f7c78b2e392c1af5b115
mizon <mizon9@gmail.com>
2013-11-03 16:56:52 +0900

    Move block parsers to toplevel

--- src/wikirick2/parsers.clj
+++ src/wikirick2/parsers.clj
@@ -5,8 +5,21 @@
             [zetta.combinators :as c]
             [zetta.parser.seq :as s]))
 
+(declare wiki)
+
 (defn scan-wiki-links [wiki-source]
   (set (map second (re-seq #"\[\[(.+?)\]\]" wiki-source))))
+
+(defn render-wiki-source [wiki-source]
+  (let [source (string/split-lines wiki-source)
+        result (parse-once wiki source)
+        value (:result result)]
+    (if value
+      value
+      result)))
+
+(defmacro def- [& forms]
+  `(def ^:private ~@forms))
 
 (defn- match? [reg]
   (s/satisfy? #(re-matches reg %)))
@@ -17,39 +30,36 @@
               (err-fn input more0 stack msg))]
       (parser input more err-fn0 ok-fn))))
 
-(def ^:private plain-line
+(def- plain-line
   (match? #"[^#>\s].*"))
 
-(def ^:private empty-line
+(def- empty-line
   (match? #"\s*"))
 
-(def ^:private wiki-parser
-  (let [headline-prefix (let [regex #"(#+) *(.*?) *#*"]
-                          (do-parser [line (match? regex)]
-                            (let [[_ syms content] (re-matches regex line)]
-                              [(keyword (str "h" (count syms))) content])))
+(def- headline-prefix
+  (let [regex #"(#+) *(.*?) *#*"]
+    (do-parser [line (match? regex)]
+      (let [[_ syms content] (re-matches regex line)]
+        [(keyword (str "h" (count syms))) content]))))
 
-        headline-underline (try-parser (do-parser [content s/any-token
-                                                   underline (match? #"(=+|-+) *")]
-                                         (if (= (first underline) \=)
-                                           [:h1 content]
-                                           [:h2 content])))
+(def- headline-underline
+  (try-parser (do-parser [content s/any-token
+                          underline (match? #"(=+|-+) *")]
+                (if (= (first underline) \=)
+                  [:h1 content]
+                  [:h2 content]))))
 
-        paragraph (do-parser [lines (c/many1 plain-line)]
-                    [:p (string/join " " lines)])
+(def- paragraph
+  (do-parser [lines (c/many1 plain-line)]
+    [:p (string/join " " lines)]))
 
-        block (reduce <|> [headline-prefix
-                           headline-underline
-                           paragraph])]
-    (do-parser [bs (c/many (*> (c/many empty-line) block))
-                _ (c/many empty-line)
-                _ s/end-of-input]
-      bs)))
+(def- block
+  (reduce <|> [headline-prefix
+               headline-underline
+               paragraph]))
 
-(defn render-wiki-source [wiki-source]
-  (let [source (string/split-lines wiki-source)
-        result (parse-once wiki-parser source)
-        value (:result result)]
-    (if value
-      value
-      result)))
+(def- wiki
+  (do-parser [bs (c/many (*> (c/many empty-line) block))
+              _ (c/many empty-line)
+              _ s/end-of-input]
+    bs))
