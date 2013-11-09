49309c6b214a247e404c661a163572479db278fa
Keita Mizuochi <mizon9@gmail.com>
2013-11-10 03:29:29 +0900

    Rewrite service test with clojure.test

---
+++ test/wikirick2/service_test.clj
@@ -0,0 +1,12 @@
+(ns wikirick2.service-test
+  (:use clojure.test)
+  (:require [wikirick2.service :as service]))
+
+(deftest wrap-with-wiki-service
+  (testing "wraps in a service"
+    (let [app (fn [req]
+                (is (= req :dummy-request))
+                (is (= wiki-service :dummy-service))
+                :dummy-response)
+          wrapped-app (service/wrap-with-wiki-service app :dummy-service)]
+      (is (= (wrapped-app :dummy-request) :dummy-response)))))
