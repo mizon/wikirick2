2b1e490c37c9709de261667340def2b7c513f4d4
Keita Mizuochi <mizon9@gmail.com>
2013-11-10 15:37:25 +0900

    Update indent.el following the codes in this project

--- indent.el
+++ indent.el
@@ -1,10 +1,5 @@
 (define-clojure-indent
   (do-parser 1)
-  (describe 1)
-  (it 1)
-  (before 0)
-  (after 0)
-  (around 0)
+  (testing-repo 1)
   (db-transaction 1)
-  (db-do-commands 1)
-  (cond 0))
+  (db-do-commands 1))
