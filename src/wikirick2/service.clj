ed3e47312e5d8c00aef7a728d886faeb3c40ce7a
Keita Mizuochi <mizon9@gmail.com>
2013-11-18 22:43:18 +0900

    Update the service ns: Rename the dynamic var and fix to construct the screen correctly

--- src/wikirick2/service.clj
+++ src/wikirick2/service.clj
@@ -1,17 +1,18 @@
 (ns wikirick2.service
   (:use wikirick2.types)
-  (:require [wikirick2.repository :as repository]
+  (:require [wikirick2.parsers :as parsers]
+            [wikirick2.repository :as repository]
             [wikirick2.screen :as screen]
             [wikirick2.url-mapper :as url-mapper]))
 
-(def ^:dynamic wiki-service nil)
+(def ^:dynamic *wiki-service* nil)
 
 (defn ws [key]
-  (key wiki-service))
+  (key *wiki-service*))
 
 (defn wrap-with-wiki-service [app service]
   (fn [req]
-    (binding [wiki-service service]
+    (binding [*wiki-service* service]
       (app req))))
 
 (defn make-wiki-service [config]
@@ -20,5 +21,7 @@
                                             :subprotocol "sqlite"
                                             :subname (config :sqlite-path)})
         urlm (url-mapper/->URLMapper (config :base-path))
-        service (map->WikiService {:config config :repository repo :url-mapper urlm})]
-    (assoc service :screen (screen/->Screen service))))
+        screen (screen/->Screen urlm
+                                (parsers/make-wiki-source-renderer #(page-path urlm %))
+                                config)]
+    (map->WikiService {:config config :repository repo :url-mapper urlm :screen screen})))
