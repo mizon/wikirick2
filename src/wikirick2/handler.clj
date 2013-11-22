b87275082a69840986ad5b29980695371ddee86d
Keita Mizuochi <mizon9@gmail.com>
2013-11-22 18:44:05 +0900

    Implement some page handlers and add tests for them

--- src/wikirick2/handler.clj
+++ src/wikirick2/handler.clj
@@ -1,24 +1,50 @@
 (ns wikirick2.handler
   (:use compojure.core
+        slingshot.slingshot
         wikirick2.service
         wikirick2.types)
   (:require [compojure.handler :as handler]
             [compojure.route :as route]
+            [ring.util.response :as response]
             [wikirick2.screen :as screen]))
 
 (defn- open-read-view [title]
   (with-wiki-service
-    (let [page (select-page repository title)]
-      (read-view screen page))))
+    (try+
+     (let [page (select-page repository title)]
+       (read-view screen page))
+     (catch [:type :page-not-found] _
+       (response/redirect (page-action-path url-mapper title "new"))))))
+
+(defn- open-new-view [title]
+  (with-wiki-service
+    (new-view screen (assoc (new-page repository title) :source "new content"))))
 
 (defn- open-edit-view [title]
   (with-wiki-service
-    (let [page (select-page repository title)]
-      (edit-view screen page))))
+    (try+
+     (let [page (select-page repository title)]
+       (edit-view screen page))
+     (catch [:type :page-not-found] _
+       (response/redirect (page-action-path url-mapper title "new"))))))
+
+(defn- register-new-page [{:keys [title source]}]
+  (with-wiki-service
+    (save-page (assoc (new-page repository title) :source source))))
+
+(defn- update-page [{:keys [title source base-ver]}]
+  (with-wiki-service
+    (let [base-ver- (Integer/parseInt base-ver)
+          page (select-page-by-version repository title base-ver-)]
+      (save-page (assoc page :source source))
+      (response/redirect-after-post (page-path url-mapper title)))))
 
 (defroutes wikirick-routes
   (GET "/" [] (open-read-view "FrontPage"))
   (GET "/w/:title" [title] (open-read-view title))
+  (GET "/w/:title/new" [title] (open-new-view title))
+  (POST "/w/:title/new" {params :params} (register-new-page params))
   (GET "/w/:title/edit" [title] (open-edit-view title))
+  (POST "/w/:title/edit" {params :params} (update-page params))
   (route/resources "/")
   (route/not-found "Not Found"))
--- test/wikirick2/handler_test.clj
+++ test/wikirick2/handler_test.clj
@@ -8,6 +8,7 @@
   (:require [clojure.java.shell :as shell]
             [compojure.core :as compojure]
             [compojure.handler :as handler]
+            [ring.util.response :as response]
             [wikirick2.screen :as screen]))
 
 (compojure/defroutes app
@@ -17,19 +18,59 @@
 
 (def repo (.repository testing-service))
 
-(deftest application-handler
-  (let [front-page (new-page repo "FrontPage" "front page content")]
+(deftest wikirick-routes-
+  (let [front-page (assoc (new-page repo "FrontPage") :source "some content")]
     (save-page front-page)
 
     (testing "handles GET /"
       (let [res (app (request :get "/"))]
         (is (= (res :status) 200)))))
 
-  (let [foo-page (new-page repo "FooPage" "some content")
-        bar-page (new-page repo "FooPage" "some content")]
+  (let [foo-page (assoc (new-page repo "FooPage") :source "some content")
+        bar-page (assoc (new-page repo "FooPage") :source "some content")]
     (save-page foo-page)
     (save-page bar-page)
 
     (testing "handles GET /w/FooPage"
-      (let [res (app (request :get "/w/FooPage"))]
-        (is (= (res :status) 200))))))
+      (with-wiki-service
+        (let [res (app (request :get "/w/FooPage"))]
+          (is (= (res :status) 200))
+          (is (= (res :body)
+                 (read-view screen (select-page repository "FooPage")))))))
+
+    (testing "handles GET /w/SomePage/new"
+      (with-wiki-service
+        (let [res (app (request :get "/w/SomePage/new"))]
+          (is (= (res :status) 200))
+          (is (= (res :body)
+                 (new-view screen
+                           (assoc (new-page repository "SomePage") :source "new content")))))))
+
+    (testing "handles GET /w/FooPage/edit"
+      (with-wiki-service
+        (let [res (app (request :get "/w/FooPage/edit"))]
+          (is (= (res :status) 200))
+          (is (= (res :body)
+                 (edit-view screen
+                            (select-page repository "FooPage")))))))
+
+    (testing "redirects when GET /w/SomePage"
+      (let [res (app (request :get "/w/SomePage"))]
+        (is (= (res :status) 302))
+        (prn res)
+        (is (= ((res :headers) "Location") "/w/SomePage/new"))))
+
+    (testing "redirects when GET /w/SomePage/edit"
+      (let [res (app (request :get "/w/SomePage/edit"))]
+        (is (= (res :status) 302))
+        (is (= ((res :headers) "Location") "/w/SomePage/new"))))
+
+    (testing "handles POST /w/FooPage/edit"
+      (let [page-content "some content"
+            res (app (request :post "/w/FooPage/edit"
+                              {:source page-content
+                               :base-ver "1"}))]
+        (is (= (res :status)))
+        (let [foo-page (select-page repo "FooPage")]
+          (is (= (page-source foo-page) page-content))
+          (is (= ((res :headers) "Location") "/w/FooPage")))))))
