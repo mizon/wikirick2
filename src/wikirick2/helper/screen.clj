2a4cc30c4787fb72aab5dd2ff35ccfee984e10ab
Keita Mizuochi <mizon9@gmail.com>
2013-12-15 18:37:03 +0900

    Make to show time zones

--- src/wikirick2/helper/screen.clj
+++ src/wikirick2/helper/screen.clj
@@ -39,7 +39,7 @@
   [:nav.page-actions [:ul [:li "Read"] [:li "Edit"] [:li "Diff"] [:li "History"]]])
 
 (defn show-date [date]
-  (format/unparse (format/formatter "yyyy/MM/dd HH:mm") date))
+  (format/unparse (format/formatter "yyyy/MM/dd HH:mm z") date))
 
 (defn show-modified-at [page revision]
   (show-date (modified-at page revision)))
